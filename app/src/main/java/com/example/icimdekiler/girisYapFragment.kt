82b8fdc4cb3a1802b455f91cb9af446db798533d
Furkan UTAR <furkan.34utar@gmail.com>
2025-02-21 23:25:50 +0300

    "Kayıt Ol ve Giriş Yap" Sayfaları Kodlandı

--- app/src/main/java/com/example/icimdekiler/girisYapFragment.kt
+++ app/src/main/java/com/example/icimdekiler/girisYapFragment.kt
@@ -5,19 +5,76 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.Navigator
+import com.example.icimdekiler.databinding.FragmentGirisYapBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import kotlin.concurrent.timerTask
 
 class girisYapFragment : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
+
+    private var _binding: FragmentGirisYapBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        auth = Firebase.auth
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_giris_yap, container, false)
+        _binding = FragmentGirisYapBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.kayitOlLabel.setOnClickListener {
+            val action = girisYapFragmentDirections.actionGirisYapFragmentToKayitOlFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
+
+        binding.girisYapButton.setOnClickListener {
+            girisYap()
+
+            if(binding.beniHatirlaCheckBox.isChecked){
+                val guncelKullanici=auth.currentUser
+                if(guncelKullanici != null){
+                    Toast.makeText(requireContext(), "Hatırladım", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    fun girisYap(){
+        val kullaniciAdi=binding.kullaniciAdiText.text.toString()
+        val ePosta=binding.ePostaText.text.toString()
+        val parola=binding.parolaText.text.toString()
+
+        if(!ePosta.isNullOrEmpty() && !parola.isNullOrEmpty()){
+            auth.signInWithEmailAndPassword(ePosta,parola).addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    Toast.makeText(requireContext(), "Hoşgeldin Kullanıcı", Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener { exeption ->
+                Toast.makeText(requireContext(), exeption.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        } else{
+            Toast.makeText(requireContext(), "Lütfen boş alan bırakmayınız!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/icimdekiler/kayitOlFragment.kt
+++ app/src/main/java/com/example/icimdekiler/kayitOlFragment.kt
@@ -5,18 +5,104 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import com.example.icimdekiler.databinding.FragmentKayitOlBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.userProfileChangeRequest
+import com.google.firebase.firestore.firestore
 
 class kayitOlFragment : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
+    val db = Firebase.firestore
+
+    private var _binding: FragmentKayitOlBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kayit_ol, container, false)
+        _binding = FragmentKayitOlBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.kayitOlButton.setOnClickListener {
+            kayitOl()
+        }
+
+        binding.girisYapLabel.setOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    fun kayitOl(){
+        val kullaniciAdi=binding.kullaniciAdiText.text.toString()
+        val isimSoyisim=binding.isimSoyisimText.text.toString()
+        val ePosta=binding.ePostaText.text.toString()
+        val telNo=binding.telNoText.text.toString()
+        val parola=binding.parolaText.text.toString()
+
+        if (kullaniciAdi.isNotEmpty() && isimSoyisim.isNotEmpty() && ePosta.isNotEmpty() && telNo.isNotEmpty() && parola.isNotEmpty()){
+            auth.createUserWithEmailAndPassword(ePosta,parola)
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+
+                        val guncelKullanici=auth.currentUser
+
+                        if(guncelKullanici != null){
+
+                            val kullaniciMap= hashMapOf<String,Any>()
+                            kullaniciMap.put("kullaniciAdi",kullaniciAdi)
+                            kullaniciMap.put("isimSoyisim",isimSoyisim)
+                            kullaniciMap.put("ePosta",ePosta)
+                            kullaniciMap.put("telNo",telNo)
+                            kullaniciMap.put("parola",parola)
+                            kullaniciMap.put("isAdmin",false)
+                            kullaniciMap.put("kullaniciUID",guncelKullanici.uid)
+
+                            db.collection("kullaniciBilgileri")
+                                .add(kullaniciMap)
+                                .addOnFailureListener { exeption ->
+                                    Toast.makeText(requireContext(), exeption.localizedMessage, Toast.LENGTH_LONG).show()
+                                }
+
+                            /*
+                            val profilGuncellemeIstegi = userProfileChangeRequest {
+                                displayName = kullaniciAdi
+                            }
+                            guncelKullanici.updateProfile(profilGuncellemeIstegi).addOnCompleteListener { task ->
+                                if(task.isSuccessful){
+                                    println("Kullanıcı Adı Güncellendi")
+                                }
+                            }
+                             */
+                        }
+                        val action = kayitOlFragmentDirections.actionKayitOlFragmentToKullaniciAnaSayfaFragment()
+                        Navigation.findNavController(requireView()).navigate(action)
+                    }
+                }.addOnFailureListener { exeption ->
+                    Toast.makeText(requireContext(), exeption.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+        }else{
+            Toast.makeText(requireContext(), "Lütfen boş alan bırakmayınız!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
