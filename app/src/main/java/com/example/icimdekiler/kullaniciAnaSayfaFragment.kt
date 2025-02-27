23b514d9daaf96b050b179d2fc24be93a9c02916
MustafaCanKaya335 <mustfcankya09@gmail.com>
2025-02-27 20:53:35 +0300

    "Kullanıcı Ana Sayfa" ve "Ürün" Sayfaları Tasarlandı

--- app/src/main/java/com/example/icimdekiler/kullaniciAnaSayfaFragment.kt
+++ app/src/main/java/com/example/icimdekiler/kullaniciAnaSayfaFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.icimdekiler.databinding.FragmentKullaniciAnaSayfaBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -12,6 +16,7 @@ import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
 
 class kullaniciAnaSayfaFragment : Fragment() {
+
 
     //Binding
     private var _binding: FragmentKullaniciAnaSayfaBinding? = null
@@ -34,6 +39,41 @@ class kullaniciAnaSayfaFragment : Fragment() {
         val view = binding.root
         return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val popupMenu = PopupMenu(requireContext(), binding.popupMenu)
+        popupMenu.menuInflater.inflate(R.menu.menu_fab, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.cikisYap -> {
+                    auth.signOut()
+
+                    val navOptions = NavOptions.Builder()
+                        .setPopUpTo(R.id.kullaniciAnaSayfaFragment, true) // kullanıcıAnaSayfaFragment'i yığından kaldır
+                        .setLaunchSingleTop(true) // Tek seferlik aç
+                        .build()
+                    findNavController().navigate(R.id.action_kullaniciAnaSayfaFragment_to_girisYapFragment, null, navOptions)
+
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.popupMenu.setOnClickListener {
+            popupMenu.show()
+        }
+
+        /*binding.barkodOkuImage3.setOnClickListener {
+            val action = adminAnaSayfaFragmentDirections.actionAdminAnaSayfaFragmentToUrunEkleFragment()
+            Navigation.findNavController(view).navigate(action)
+        }*/
+
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
